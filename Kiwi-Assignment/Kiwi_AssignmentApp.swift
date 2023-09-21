10151da27dabdcfe469f8860e47101fb87427fbb
Vyacheslavskiy <nagornyak.slava@gmail.com>
2023-09-21 10:17:39 +0200

    Initial Commit

---
+++ Kiwi-Assignment/Kiwi_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kiwi_AssignmentApp.swift
+//  Kiwi-Assignment
+//
+//  Created by Slava Nagornyak on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Kiwi_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
